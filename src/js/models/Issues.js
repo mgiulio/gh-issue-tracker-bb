f5f3126981afd8fb5e4c72e34e2ad32ec15904c9
mgiulio <giulio.mainardi@gmail.com>
2015-05-28 18:02:20 +0200

    Pagination: use GH API provided Link headers

--- src/js/models/Issues.js
+++ src/js/models/Issues.js
@@ -3,25 +3,27 @@ var Issues = (function() {
 	var Collection = Backbone.Collection.extend({
 		
 		model: Issue,
+		paginationUrls: {},
 		
 		initialize: function(models, options) {
 		},
 		
 		setRepo: function(repo) {
 			this.repo = repo;
-			this.page = 0;
+			
+			this.paginationUrls['next'] =  `https://api.github.com/repos/${this.repo}/issues?state=all`;
 			
 			this.fetchNextPage();
 		},
 		
 		fetchNextPage: function() {
-			var nextPageUrl = this.getNextPageUrl();
+			var nextPageUrl = this.paginationUrls['next'];
 			
 			var self = this;
 			this.fetch({
 				url: nextPageUrl, 
-				success: function() {
-					self.page++; 
+				success: function(collection, models, options) {
+					self.updatePaginationUrls(options.xhr.getAllResponseHeaders());
 					self.url = nextPageUrl;
 				},
 				error: function() {
@@ -30,9 +32,17 @@ var Issues = (function() {
 				remove: false
 			});
 		},
-		
-		getNextPageUrl: function() {
-			return `https://api.github.com/repos/${this.repo}/issues?state=all&page=${this.page + 1}`;
+	
+		updatePaginationUrls: function(httpHeadersString) {
+			var headers = http.headerObject(httpHeadersString.trim());
+			
+			var self = this;
+			headers['Link'].split(',').forEach(function(link) {
+				var parts = link.trim().split(';');
+				var url = parts[0].slice(1, -1);
+				var rel = parts[1].trim().split('=')[1].slice(1, -1);
+				self.paginationUrls[rel] = url;
+			});
 		}
 		
 	});
